517345cafb8760ff6361546296dc53c5fa1b63c5
dilaragorum <dilaragorum@gmail.com>
2022-04-05 15:31:04 +0300

    refactor

--- main.go
+++ main.go
@@ -9,39 +9,53 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-
+const (
+	fizzBuzz = "fizzBuzz"
+	fizz     = "fizz"
+	buzz     = "buzz"
+)
 
 func FizzBuzz(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// Get metodu dışında başka metoda izin vermiyoruz
 	var fizzBuzzArray []string
 	count, err := strconv.Atoi(ps.ByName("count"))
-	// count değeri 0'dan küçük veya int'e çevrilemez ise  
-	if err != nil || count <= 0 {
+	// count değeri 0'dan küçük veya int'e çevrilemez ise
+	if err != nil {
 		http.NotFound(w, r)
+		return
+	}
+	if count <= 0 {
+		http.Error(w, "", http.StatusBadRequest)
 		return
 	}
 	// fizzBuzz Algorithm
 	for i := 1; i <= count; i++ {
 		if i%3 == 0 && i%5 == 0 {
-			fizzBuzzArray = append(fizzBuzzArray, "fizzBuzz")
+			fizzBuzzArray = append(fizzBuzzArray, fizzBuzz)
 		} else if i%5 == 0 {
-			fizzBuzzArray = append(fizzBuzzArray, "buzz")
+			fizzBuzzArray = append(fizzBuzzArray, buzz)
 		} else if i%3 == 0 {
-			fizzBuzzArray = append(fizzBuzzArray, "fizz")
+			fizzBuzzArray = append(fizzBuzzArray, fizz)
 		} else {
 			fizzBuzzArray = append(fizzBuzzArray, strconv.Itoa(i))
 		}
 	}
 	// Array'i json'a çevirip header ile birlikte yazdırmak.
-	jsonStr, _ := json.Marshal(fizzBuzzArray)
+	jsonStr, err := json.Marshal(fizzBuzzArray)
+	if err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonStr)
 }
 
 func main() {
 	router := httprouter.New()
-	router.GET("/fizzbuzz/:count", FizzBuzz)
+	router.GET("/fizzBuzz/:count", FizzBuzz)
 
 	err := http.ListenAndServe(":8080", router)
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
